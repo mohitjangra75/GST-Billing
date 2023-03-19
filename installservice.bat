4e9f000a49e20b2eacfe1f6e2f08f4b6fed54679
MOHIT <84301563+mohitjangra75@users.noreply.github.com>
2023-03-19 13:29:53 +0530

    Delete installservice.bat

--- installservice.bat
+++
@@ -1,4 +0,0 @@
-sc stop "GSTBILLING"
-sc delete "GSTBILLING"
-sc create "GSTBILLING" start= auto displayname= "GST Billing Service" binpath= "%~dp0\python-3.8.1-embed-amd64\python.exe %~dp0\manage.py runserver"
-sc start "GSTBILLING"
